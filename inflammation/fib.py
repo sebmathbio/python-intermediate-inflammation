57a13463061d97491f494f18c94622f0bc8f7793
sebmathbio <sebgilbert@protonmail.com>
2025-04-01 14:36:57 +0100

    Rewirte docstring in the sphynx docstring style.

--- inflammation/fib.py
+++ inflammation/fib.py
@@ -1,19 +1,16 @@
 def fibonacci(n):
-    """Calculate the nth Fibonacci number.
+    """
+    Calculate the nth Fibonacci number.
 
-    A recursive implementation of Fibonacci array elements.
-
-    Params
-     ----
-     n: integer
-
-    Raises
-     ----
-    ValueError: raised if n is less than zero
-
-    Returns
-     ----
-     Fibonacci number
+    :param n: The index of the Fibonacci number to calculate.
+    :type n: int
+    :return: The nth Fibonacci number.
+    :rtype: int
+    :raises ValueError: If n is less than zero.
+    
+    :notes:
+        This function uses a recursive implementation to calculate the Fibonacci number.
+        The Fibonacci sequence starts with 0 and 1, and each subsequent number is the sum of the two previous numbers.
     """
     if n < 0:
         raise ValueError('Fibonacci is not defined for N < 0')
